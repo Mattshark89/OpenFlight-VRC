70b50f6ec9ba9b79b40a0ad17c10aedbd0ae5b54
BuildTools <unconfigured@null.spigotmc.org>
2022-12-20 18:56:16 -0700

    more public settings, improve flapping from ground

--- AvaliFlight.cs
+++ AvaliFlight.cs
@@ -6,25 +6,31 @@ using VRC.Udon;
 
 public class AvaliFlight : UdonSharpBehaviour {
     private VRCPlayerApi LocalPlayer;
-    [Tooltip("Make this stronk")]
-    public float flightStrength = 300;
+    [Tooltip("Strength of each flap. Recommended values for default gravity: 300-700 (Default: 400)")]
+    public float flightStrength = 400f;
     private Vector3 RHPos;
     private Vector3 LHPos;
     private Vector3 RHPosLast;
     private Vector3 LHPosLast;
     private bool isFlapping;
     private bool isFlying;
-    public bool setGravity;
-    private float oldGravity;
-    public float gravity;
-    private Vector3 speedMod;
+    [Tooltip("Change gravity while flying? (Default: true)")]
+    public bool setGravity = true;
+    [Tooltip("Value of gravity while flying (Default: 0.22)")]
+    public float gravity = 0.22f;
+    [Tooltip("Allow locomotion (wasd/left joystick) while flying? (Default: false)")]
+    public bool allowLoco;
+    private Vector3 velMod;
+    // "old" values are the world's defaults
+    private float oldGravityStrength;
+    private float oldWalkSpeed;
+    private float oldRunSpeed;
+    private float oldStrafeSpeed;
     
     public void Start() {
         isFlapping = false;
+        isFlying = false;
         LocalPlayer = Networking.LocalPlayer;
-        // Avoid errors with RHPosLast not existing
-        RHPosLast = LocalPlayer.GetPosition() - LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.RightHand).position;
-        LHPosLast = LocalPlayer.GetPosition() - LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.LeftHand).position;
     }
     
     public void Update() {
@@ -36,31 +42,57 @@ public class AvaliFlight : UdonSharpBehaviour {
                 RHPos = LocalPlayer.GetPosition() - LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.RightHand).position;
                 LHPos = LocalPlayer.GetPosition() - LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.LeftHand).position;
                 // Calculate Force to apply
-                speedMod = ((RHPos - RHPosLast) + (LHPos - LHPosLast)) * (Time.deltaTime * flightStrength);
+                velMod = ((RHPos - RHPosLast) + (LHPos - LHPosLast)) * (Time.deltaTime * flightStrength);
                 RHPosLast = RHPos;
                 LHPosLast = LHPos;
 
-                LocalPlayer.SetVelocity(LocalPlayer.GetVelocity() + speedMod);
+                LocalPlayer.SetVelocity(LocalPlayer.GetVelocity() + velMod);
             } else { // First frame of flapping (setting necessary variables)
                 isFlapping = true;
-                if (!isFlying) {
+                velMod = LocalPlayer.GetVelocity();
+                if (!isFlying) { // First flap of the flight (ie grounded)
                     isFlying = true;
                     if (setGravity) {
-                        oldGravity = LocalPlayer.GetGravityStrength();
+                        oldGravityStrength = LocalPlayer.GetGravityStrength();
                         LocalPlayer.SetGravityStrength(gravity);
                     }
+                    if (!allowLoco) {
+                        ImmobilizePart(true);
+                    }
+                    // Workaround to get the player off the ground
+                    velMod.y = LocalPlayer.GetGravityStrength() * Time.deltaTime * 500;
                 }
                 RHPosLast = LocalPlayer.GetPosition() - LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.RightHand).position;
                 LHPosLast = LocalPlayer.GetPosition() - LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.LeftHand).position;
+                // (pseudocode `LocalPlayer.PlayerGrounded(false)`)
+                LocalPlayer.SetVelocity(velMod);
             }
         } else { // Stopped flapping
             isFlapping = false;
         }
-        if (isFlying && LocalPlayer.IsPlayerGrounded()) {
+        if (isFlying && LocalPlayer.IsPlayerGrounded()) { // Script to run when landing
             isFlying = false;
             if (setGravity) {
-                LocalPlayer.SetGravityStrength(oldGravity);
+                LocalPlayer.SetGravityStrength(oldGravityStrength);
             }
+            if (!allowLoco) {
+                ImmobilizePart(false);
+            }
+        }
+    }
+    // Immobilize Locomotion but still allow body rotation
+    private void ImmobilizePart(bool b) {
+        if (b) {
+            oldWalkSpeed = LocalPlayer.GetWalkSpeed();
+            oldRunSpeed = LocalPlayer.GetRunSpeed();
+            oldStrafeSpeed = LocalPlayer.GetStrafeSpeed();
+            LocalPlayer.SetWalkSpeed(0f);
+            LocalPlayer.SetRunSpeed(0f);
+            LocalPlayer.SetStrafeSpeed(0f);
+        } else {
+            LocalPlayer.SetWalkSpeed(oldWalkSpeed);
+            LocalPlayer.SetRunSpeed(oldRunSpeed);
+            LocalPlayer.SetStrafeSpeed(oldStrafeSpeed);
         }
     }
 }
