729f3bb790cfd39e3fa2ad61b099ab780f404b73
Happyrobot33 <matthewherber@sbcglobal.net>
2023-03-28 17:05:30 -0400

    Fix defaults for weight value being incorrect
    This led to weird physics on forced on mode

--- Assets/OpenFlight-VRC/Scripts/AvatarDetection.cs
+++ Assets/OpenFlight-VRC/Scripts/AvatarDetection.cs
@@ -49,7 +49,7 @@ public class AvatarDetection : UdonSharpBehaviour
     public GameObject wingtipGizmo; //this shows the wingtip offset as a sphere in game. Only works in VR due to implementation
 
     //information about the avatar that has been detected
-    public float weight = 0;
+    public float weight = 1;
     public float WingtipOffset = 0;
     public string name = ""; //this is the name of the avatar base
     public string creator = ""; //this is the person who created the avatar base
@@ -126,7 +126,7 @@ public class AvatarDetection : UdonSharpBehaviour
                 name = "Loading Avatar";
                 creator = "Loading Avatar";
                 introducer = "Loading Avatar";
-                weight = 0;
+                weight = 1;
                 WingtipOffset = 0;
                 return;
             }
