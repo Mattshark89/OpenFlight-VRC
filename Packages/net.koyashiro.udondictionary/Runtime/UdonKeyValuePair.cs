0a9e18d92d9fbb73b06da2bf887d2110da2fec51
Matthew Herber <32679887+Happyrobot33@users.noreply.github.com>
2023-04-10 13:33:57 -0400

    remove according to VRC GIT standards
    https://vcc.docs.vrchat.com/vpm/source-control

--- Packages/net.koyashiro.udondictionary/Runtime/UdonKeyValuePair.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using UdonSharp;
-
-namespace Koyashiro.UdonDictionary
-{
-    [AddComponentMenu("")]
-    public class UdonKeyValuePair<TKey, TValue> : UdonSharpBehaviour { }
-}
