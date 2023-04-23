745d5c13062674ffcc85f81a81fe42f3553301ed
Happyrobot33 <matthewherber@sbcglobal.net>
2023-04-23 02:15:13 -0400

    Re-Implement version updating on tablet with new editor based method
    This essentially injects the version number upon building the scene itself

--- Packages/com.mattshark.openflight/Runtime/Scripts/Flight/OpenFlight.cs
+++ Packages/com.mattshark.openflight/Runtime/Scripts/Flight/OpenFlight.cs
@@ -5,11 +5,34 @@ using Unity.Collections;
 using VRC.SDKBase;
 using VRC.Udon;
 
+//This chunk of code allows the OpenFlight version number to be set automatically from the package.json file
+//its done using this method for dumb unity reasons but it works so whatever
+#if !COMPILER_UDONSHARP && UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class OpenFlightScenePostProcessor {
+	[PostProcessSceneAttribute]
+	public static void OnPostProcessScene() {
+		//get the openflight version from the package.json file
+		string packageJson = System.IO.File.ReadAllText("Packages/com.mattshark.openflight/package.json");
+		string version = packageJson.Split(new string[] { "\"version\": \"" }, System.StringSplitOptions.None)[1].Split('"')[0];
+		//find all the OpenFlight scripts in the scene
+		OpenFlight[] openFlightScripts = Object.FindObjectsOfType<OpenFlight>();
+		foreach (OpenFlight openFlightScript in openFlightScripts)
+		{
+			//set their version number
+			openFlightScript.OpenFlightVersion = version;
+		}
+	}
+}
+#endif
+
 public class OpenFlight : UdonSharpBehaviour
 {
 	//this removes any override that the editor might have set through the inspector ([HideInInspector] does NOT do that)
 	[System.NonSerialized]
-	public string OpenFlightVersion = "1.2.6";
+	public string OpenFlightVersion = "?.?.?";
 	public GameObject wingedFlight;
 	public AvatarDetection avatarDetection;
 	public string flightMode = "Auto";
