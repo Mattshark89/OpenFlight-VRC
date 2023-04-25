8890800080dea05af17b1b827a88a7efe29618cf
Matthew Herber <32679887+Happyrobot33@users.noreply.github.com>
2023-04-25 10:46:54 -0400

    Change package id, implement dropdown prefab adding

---
+++ Packages/com.mattshark.openflight.extensions/Editor/OpenFlight Extensions Window.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class OpenFlightExtensionsWindow : EditorWindow
+{
+	[MenuItem("VRC Packages/OpenFlight/Prefabs/Zones/ZoneNotifier")]
+	public static void ZoneNotifier()
+	{
+		GameObject prefab =
+			AssetDatabase.LoadAssetAtPath("Packages/com.mattshark.openflight.extensions/Runtime/Prefabs/ZoneNotifier.prefab", typeof(GameObject)) as GameObject;
+		AddPrefabToScene(prefab, true);
+	}
+
+	[MenuItem("VRC Packages/OpenFlight/Prefabs/Zones/No Fly Zone")]
+	public static void NoFlyZone()
+	{
+		//check if a zone notifier is already in the scene
+		//if it isnt, add it
+		if (!GameObject.Find("ZoneNotifier"))
+		{
+			ZoneNotifier();
+		}
+		GameObject prefab =
+			AssetDatabase.LoadAssetAtPath("Packages/com.mattshark.openflight.extensions/Runtime/Prefabs/NoFlyZone.prefab", typeof(GameObject)) as GameObject;
+		AddPrefabToScene(prefab);
+	}
+
+	[MenuItem("VRC Packages/OpenFlight/Prefabs/Zones/Defaults Zone")]
+	public static void DefaultsZone()
+	{
+		//check if a zone notifier is already in the scene
+		//if it isnt, add it
+		if (!GameObject.Find("ZoneNotifier"))
+		{
+			ZoneNotifier();
+		}
+		GameObject prefab =
+			AssetDatabase.LoadAssetAtPath("Packages/com.mattshark.openflight.extensions/Runtime/Prefabs/DefaultsZone.prefab", typeof(GameObject)) as GameObject;
+		AddPrefabToScene(prefab);
+	}
+
+	static void AddPrefabToScene(GameObject prefab, bool checkForExisting = false)
+	{
+		// Check to see if the prefab is already in the scene
+		if (GameObject.Find(prefab.name) && checkForExisting)
+		{
+			EditorUtility.DisplayDialog("Prefab already in scene", "The prefab is already in the scene, so it wont be added", "OK");
+			return;
+		}
+		GameObject instance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+	}
+}
