f177c893fdec8f2ee133af6738e2a898f277b893
Marty <github@katserv.net>
2023-05-01 21:14:41 -0700

    Basic updraft script and asset

---
+++ Packages/com.mattshark.openflight/Runtime/Scripts/Flight/Updraft.cs
@@ -0,0 +1,53 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Updraft : UdonSharpBehaviour
+{
+    [Range(0,800)]
+    public int updraftStrength;
+
+    public GameObject wingedFlight;
+    private WingFlightPlusGlide flightCore;
+    private bool Enabled = false;
+
+    void Start()
+    {
+        if (wingedFlight != null)
+        {
+            flightCore = wingedFlight.GetComponent<WingFlightPlusGlide>();
+            Enabled = true;
+        }
+        else
+        {
+            Enabled = false;
+            Debug.LogError("Disabling Updraft - Script missing WingedFlight GameObject");
+        }
+    }
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (Enabled && player.IsValid() && player.isLocal)
+        {
+            if (flightCore != null)
+            {
+                flightCore.EnterUpdraft(updraftStrength);
+            }
+        }
+        base.OnPlayerTriggerEnter(player);
+    }
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        if (Enabled && player.IsValid() && player.isLocal)
+        {
+            if (flightCore != null)
+            {
+                flightCore.ExitUpdraft();
+            }
+        }
+        base.OnPlayerTriggerExit(player);
+    }
+}
