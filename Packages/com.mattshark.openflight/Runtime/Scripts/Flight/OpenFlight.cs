5d285a52e9c94bfe55ff5608174beec8bcf2fbea
Matthew Herber <32679887+Happyrobot33@users.noreply.github.com>
2023-05-03 10:33:12 -0400

    Fix infinite loop

--- Packages/com.mattshark.openflight/Runtime/Scripts/Flight/OpenFlight.cs
+++ Packages/com.mattshark.openflight/Runtime/Scripts/Flight/OpenFlight.cs
@@ -13,6 +13,7 @@ namespace OpenFlightVRC
 using UnityEditor;
 using UnityEditor.Callbacks;
 
+
 public class OpenFlightScenePostProcessor {
 	[PostProcessSceneAttribute]
 	public static void OnPostProcessScene() {
@@ -29,23 +30,7 @@ public class OpenFlightScenePostProcessor {
 	}
 }
 #endif
-
-public class OpenFlight : UdonSharpBehaviour
-{
-	//this removes any override that the editor might have set through the inspector ([HideInInspector] does NOT do that)
-	[System.NonSerialized]
-	public string OpenFlightVersion = "?.?.?";
-	public GameObject wingedFlight;
-	public AvatarDetection avatarDetection;
-	public string flightMode = "Auto";
-	string flightModePrevious = "Auto";
-	private VRCPlayerApi LocalPlayer;
-
-	[ReadOnly]
-	public bool flightAllowed = false;
-	public bool flightForcedOff = false; //used for external scripts to force flight off no matter what
-
-	void SwitchFlight()
+	public class OpenFlight : UdonSharpBehaviour
 	{
 		//this removes any override that the editor might have set through the inspector ([HideInInspector] does NOT do that)
 		[System.NonSerialized]
@@ -53,73 +38,18 @@ public class OpenFlight : UdonSharpBehaviour
 		public GameObject wingedFlight;
 		public AvatarDetection avatarDetection;
 		public string flightMode = "Auto";
+		string flightModePrevious = "Auto";
 		private VRCPlayerApi LocalPlayer;
 
 		[ReadOnly]
 		public bool flightAllowed = false;
+		public bool flightForcedOff = false; //used for external scripts to force flight off no matter what
 
 		void SwitchFlight()
 		{
-			FlightOff();
-		}
-	}
-
-	public void FlightOn()
-	{
-		if (LocalPlayer.IsUserInVR() && !flightForcedOff)
-		{
-			SwitchFlight();
-			wingedFlight.SetActive(true);
-			flightMode = "On";
-			flightAllowed = true;
-		}
-	}
-
-	public void FlightOff()
-	{
-		if (!flightForcedOff)
-		{
-			SwitchFlight();
-			wingedFlight.SetActive(false);
-			flightMode = "Off";
-			flightAllowed = false;
-		}
-	}
-
-	public void FlightAuto()
-	{
-		if (LocalPlayer.IsUserInVR() && !flightForcedOff)
-		{
-			flightMode = "Auto";
-			flightAllowed = false;
-
-			//tell the avatar detection script to check if the player can fly again
-			if (avatarDetection != null)
-			{
-				avatarDetection.ReevaluateFlight();
-			}
-		}
-	}
-
-	public void CanFly()
-	{
-		if (string.Equals(flightMode, "Auto") && !flightForcedOff)
-		{
-			SwitchFlight();
-			wingedFlight.SetActive(true);
-			flightAllowed = true;
-		}
-	}
-
-	public void CannotFly()
-	{
-		if (string.Equals(flightMode, "Auto") && !flightForcedOff)
-		{
-			SwitchFlight();
 			wingedFlight.SetActive(false);
 			flightAllowed = false;
 		}
-	}
 
 		public void Start()
 		{
@@ -132,7 +62,7 @@ public class OpenFlight : UdonSharpBehaviour
 
 		public void FlightOn()
 		{
-			if (LocalPlayer.IsUserInVR())
+			if (LocalPlayer.IsUserInVR() && !flightForcedOff)
 			{
 				SwitchFlight();
 				wingedFlight.SetActive(true);
@@ -143,15 +73,18 @@ public class OpenFlight : UdonSharpBehaviour
 
 		public void FlightOff()
 		{
-			SwitchFlight();
-			wingedFlight.SetActive(false);
-			flightMode = "Off";
-			flightAllowed = false;
+			if (!flightForcedOff)
+			{
+				SwitchFlight();
+				wingedFlight.SetActive(false);
+				flightMode = "Off";
+				flightAllowed = false;
+			}
 		}
 
 		public void FlightAuto()
 		{
-			if (LocalPlayer.IsUserInVR())
+			if (LocalPlayer.IsUserInVR() && !flightForcedOff)
 			{
 				flightMode = "Auto";
 				flightAllowed = false;
@@ -166,7 +99,7 @@ public class OpenFlight : UdonSharpBehaviour
 
 		public void CanFly()
 		{
-			if (string.Equals(flightMode, "Auto"))
+			if (string.Equals(flightMode, "Auto") && !flightForcedOff)
 			{
 				SwitchFlight();
 				wingedFlight.SetActive(true);
@@ -176,7 +109,7 @@ public class OpenFlight : UdonSharpBehaviour
 
 		public void CannotFly()
 		{
-			if (string.Equals(flightMode, "Auto"))
+			if (string.Equals(flightMode, "Auto") && !flightForcedOff)
 			{
 				SwitchFlight();
 				wingedFlight.SetActive(false);
@@ -184,35 +117,35 @@ public class OpenFlight : UdonSharpBehaviour
 			}
 		}
 
-	//These are used by scripts that need to force flight on or off no matter what the player wants
-	public void ForceDisableFlight()
-	{
-		SwitchFlight();
-		wingedFlight.SetActive(false);
-		flightModePrevious = flightMode;
-		flightMode = "Forced Off";
-		flightAllowed = false;
-		flightForcedOff = true;
+		//These are used by scripts that need to force flight on or off no matter what the player wants
+		public void ForceDisableFlight()
+		{
+			SwitchFlight();
+			wingedFlight.SetActive(false);
+			flightModePrevious = flightMode;
+			flightMode = "Forced Off";
+			flightAllowed = false;
+			flightForcedOff = true;
+		}
+
+		public void ResetForcedFlight()
+		{
+			flightForcedOff = false;
+			flightMode = flightModePrevious;
+			if (string.Equals(flightMode, "Auto"))
+			{
+				FlightAuto();
+			}
+
+			if (string.Equals(flightMode, "On"))
+			{
+				FlightOn();
+			}
+
+			if (string.Equals(flightMode, "Off"))
+			{
+				FlightOff();
+			}
+		}
 	}
-
-	public void ResetForcedFlight()
-	{
-		flightForcedOff = false;
-		flightMode = flightModePrevious;
-		if (string.Equals(flightMode, "Auto"))
-		{
-			FlightAuto();
-		}
-
-		if (string.Equals(flightMode, "On"))
-		{
-			FlightOn();
-		}
-
-		if (string.Equals(flightMode, "Off"))
-		{
-			FlightOff();
-		}
-	}
-}
 }
