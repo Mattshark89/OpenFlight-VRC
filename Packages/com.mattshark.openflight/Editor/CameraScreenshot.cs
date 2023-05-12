deddd2752d6bf8078d779fb7295ca67f5529eb83
Happyrobot33 <matthewherber@sbcglobal.net>
2023-05-12 13:59:52 -0400

    Add in-editor screenshot tool

---
+++ Packages/com.mattshark.openflight/Editor/CameraScreenshot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace OpenFlightVRC.Editor
+{
+	public class CameraScreenshot : EditorWindow
+	{
+		[MenuItem("VRC Packages/OpenFlight/Editor/Camera Screenshot")]
+		public static void TakeScreenshot()
+		{
+			// ask the user where to save the screenshot
+			string path = EditorUtility.SaveFilePanel("Save Screenshot", "Assets/Screenshots", "Screenshot.png", "png");
+			if (path.Length != 0)
+			{
+				// Take a screenshot of the scene view
+				ScreenCapture.CaptureScreenshot(path);
+			}
+		}
+	}
+}
