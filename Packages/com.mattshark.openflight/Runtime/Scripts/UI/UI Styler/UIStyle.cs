536a57703bc39b27af93a4c05293722f2a9b3400
Happyrobot33 <matthewherber@sbcglobal.net>
2023-12-13 11:39:14 -0500

    Close #63

--- Packages/com.mattshark.openflight/Runtime/Scripts/UI/UI Styler/UIStyle.cs
+++ Packages/com.mattshark.openflight/Runtime/Scripts/UI/UI Styler/UIStyle.cs
@@ -17,7 +17,6 @@ namespace OpenFlightVRC.UI
 		}
 	}
 
-	[CreateAssetMenu(fileName = "UIStyle", menuName = "VRC Packages/OpenFlight/UIStyle", order = 1)]
 	internal class UIStyle : ScriptableObject, VRC.SDKBase.IEditorOnly
 	{
 		[StyleMarkupLink(StyleClass.Background)]
@@ -85,5 +84,30 @@ namespace OpenFlightVRC.UI
 
 		[StyleMarkupLink(StyleClass.InActiveTab)]
 		public Color inActiveTab = Color.white;
+
+		//custom implementation of the create asset menu to restrict the creation of the UIStyle asset to the assets folder only
+#if UNITY_EDITOR
+		[UnityEditor.MenuItem("Assets/Create/VRC Packages/OpenFlight/UI Style", priority = 100)]
+		private static void CreateUIStyle()
+		{
+			string directory = UnityEditor.AssetDatabase.GetAssetPath(UnityEditor.Selection.activeObject);
+			if (System.IO.Path.HasExtension(directory))
+				directory = System.IO.Path.GetDirectoryName(directory);
+
+			//check to make sure the directory is in the assets folder, and if not, error out with a popup
+			if (!directory.StartsWith("Assets"))
+			{
+				UnityEditor.EditorUtility.DisplayDialog("Error", "UI Style assets should only be created in the Assets folder, not in any packages", "OK");
+				return;
+			}
+			
+			UIStyle myObject = ScriptableObject.CreateInstance<UIStyle>();
+
+			string assetName = "New UI Style.asset";
+			string path = System.IO.Path.Combine(directory, assetName);
+			UnityEditor.AssetDatabase.CreateAsset(myObject, path);
+			UnityEditor.Selection.activeObject = myObject;
+		}
+#endif
 	}
 }
