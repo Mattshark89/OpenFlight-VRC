163bd6745d9eba34813fd454d847330cd98acce5
Happyrobot33 <matthewherber@sbcglobal.net>
2024-01-27 19:46:27 -0500

    switch over to new implementation of networking debug to solve lag issue

--- Packages/com.mattshark.openflight/Runtime/Scripts/UI/NetworkingDebug.cs
+++ Packages/com.mattshark.openflight/Runtime/Scripts/UI/NetworkingDebug.cs
@@ -1,5 +1,4 @@
-﻿
-using UdonSharp;
+﻿using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
@@ -10,78 +9,134 @@ using System;
 
 namespace OpenFlightVRC.UI
 {
-    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
-    public class NetworkingDebug : LoggableUdonSharpBehaviour
-    {
-        private CyanPlayerObjectAssigner Assigner;
-        public UdonBehaviour assignerProxy;
-        private TextMeshProUGUI Text;
-        void Start()
-        {
-            //get the real assigner from the proxy, if it exists
-            if (assignerProxy.GetProgramVariable("target") != null)
-            {
-                //get the assigner as a component first
-                UdonBehaviour assignerBehaviour = (UdonBehaviour)assignerProxy.GetProgramVariable("target");
-                Assigner = (CyanPlayerObjectAssigner)assignerBehaviour.GetComponent(typeof(UdonBehaviour));
-            }
+	[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+	public class NetworkingDebug : LoggableUdonSharpBehaviour
+	{
+		private CyanPlayerObjectAssigner Assigner;
+		public UdonBehaviour assignerProxy;
+		private TextMeshProUGUI Text;
 
-            Text = GetComponent<TextMeshProUGUI>();
-        }
+		void Start()
+		{
+			//get the real assigner from the proxy, if it exists
+			if (assignerProxy.GetProgramVariable("target") != null)
+			{
+				//get the assigner as a component first
+				UdonBehaviour assignerBehaviour = (UdonBehaviour)assignerProxy.GetProgramVariable("target");
+				Assigner = (CyanPlayerObjectAssigner)assignerBehaviour.GetComponent(typeof(UdonBehaviour));
+			}
 
-        void Update()
-        {
-            //query and show in a list all the information
-            string text = "";
+			Text = GetComponent<TextMeshProUGUI>();
+		}
 
-            text += "Network Clogged: " + Networking.IsClogged + "\n";
-            text += "Network Settled: " + Networking.IsNetworkSettled + "\n";
+		private string _text = "";
+		private VRCPlayerApi[] _players = new VRCPlayerApi[0];
 
-            VRCPlayerApi[] players = new VRCPlayerApi[VRCPlayerApi.GetPlayerCount()];
-            VRCPlayerApi.GetPlayers(players);
-            foreach (VRCPlayerApi player in players)
-            {
-                text += QueryForPlayer(player) + "\n";
-            }
+		/// <summary>
+		/// The current player being updated in the player list
+		/// </summary>
+		private int _currentPlayerBeingUpdated = 0;
+		private int _playerCount = 0;
 
-            Text.text = text;
-        }
+		void Update()
+		{
+			//The whole reason this code looks like this is to spread out the work over multiple frames
+			if (_currentPlayerBeingUpdated == 0)
+			{
+				//push the text from the last frame to the UI
+				Text.text = _text;
 
-        private string QueryForPlayer(VRCPlayerApi player)
-        {
-            Component behaviour = Assigner._GetPlayerPooledUdon(player);
+				//reset the text
+				_text = "";
+				_playerCount = VRCPlayerApi.GetPlayerCount();
+				_players = new VRCPlayerApi[_playerCount];
+				VRCPlayerApi.GetPlayers(_players);
+				_text += string.Format(
+					"Network CLogged: {0}\nNetwork Settled: {1}\nPlayer Count: {2}\n",
+					Networking.IsClogged,
+					Networking.IsNetworkSettled,
+					_playerCount
+				);
+			}
 
-            //if null, player is not in the pool yet
-            if (behaviour == null)
-            {
-                return player.displayName + " not in pool";
-            }
+			VRCPlayerApi player = _players[_currentPlayerBeingUpdated];
+			//check if the player is valid
+			if (player.IsValid())
+			{
+				_text += QueryForPlayer(player) + "\n";
+			}
 
-            PlayerInfoStore store = (PlayerInfoStore)behaviour;
+			_currentPlayerBeingUpdated++;
 
-            string playerHeader = "<b>" + player.displayName + "</b>";
+			//if we have updated all the players, reset the counter
+			if (_currentPlayerBeingUpdated + 1 > _playerCount)
+			{
+				_currentPlayerBeingUpdated = 0;
+			}
+		}
 
-            //estimate ping based on time since startup and simulation time
-            //in MS
-            float latency = (Time.realtimeSinceStartup - Networking.SimulationTime(player)) * 1000;
-            //round it
-            latency = Mathf.Round(latency);
+		private string QueryForPlayer(VRCPlayerApi player)
+		{
+			Component behaviour = Assigner._GetPlayerPooledUdon(player);
 
-            //get the packed data in a bit string
-            string packedData = Convert.ToString(store.PackedData, 2).PadLeft(8, '0');
+			//if null, player is not in the pool yet
+			if (behaviour == null)
+			{
+				return player.displayName + " not in pool";
+			}
 
-            const string tab = "  ";
+			PlayerInfoStore store = (PlayerInfoStore)behaviour;
 
-            return playerHeader + "\n" +
-                tab + "latency (ms): " + latency + "\n" +
-                tab + "Extracted Data:\n" +
-                tab + tab + "isFlying: " + store.IsFlying + "\n" +
-                tab + tab + "isFlapping: " + store.IsFlapping + "\n" +
-                tab + tab + "isContributer: " + store.IsContributer + "\n" +
-                tab + "Synched Data:\n" +
-                tab + tab + "Packed Data: " + "0b" + packedData + "\n" +
-                tab + tab + "WorldWingtipOffset: " + store.WorldWingtipOffset + "\n" +
-                "---------------------";
-        }
-    }
+			string playerHeader = "<b>" + player.displayName + "</b>";
+
+			//estimate ping based on time since startup and simulation time
+			//in MS
+			float latency = (Time.realtimeSinceStartup - Networking.SimulationTime(player)) * 1000;
+			//round it
+			latency = Mathf.Round(latency);
+
+			//get the packed data in a bit string
+			string packedData = Convert.ToString(store.PackedData, 2).PadLeft(8, '0');
+
+			const string tab = "  ";
+
+			return playerHeader
+				+ "\n"
+				+ tab
+				+ "latency (ms): "
+				+ latency
+				+ "\n"
+				+ tab
+				+ "Extracted Data:\n"
+				+ tab
+				+ tab
+				+ "isFlying: "
+				+ store.IsFlying
+				+ "\n"
+				+ tab
+				+ tab
+				+ "isFlapping: "
+				+ store.IsFlapping
+				+ "\n"
+				+ tab
+				+ tab
+				+ "isContributer: "
+				+ store.IsContributer
+				+ "\n"
+				+ tab
+				+ "Synched Data:\n"
+				+ tab
+				+ tab
+				+ "Packed Data: "
+				+ "0b"
+				+ packedData
+				+ "\n"
+				+ tab
+				+ tab
+				+ "WorldWingtipOffset: "
+				+ store.WorldWingtipOffset
+				+ "\n"
+				+ "---------------------";
+		}
+	}
 }
