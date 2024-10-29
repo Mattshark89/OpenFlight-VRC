e73f1e42875f2b7f9b30e65ddaa9b6538bf3465c
Happyrobot33 <matthewherber@sbcglobal.net>
2024-10-29 10:08:58 -0400

    Fix compilation issue with U# bug by moving the variable to another parent

--- Packages/com.mattshark.openflight/Runtime/Scripts/Utils/CallbackUdonSharpBehaviour.cs
+++ Packages/com.mattshark.openflight/Runtime/Scripts/Utils/CallbackUdonSharpBehaviour.cs
@@ -3,9 +3,7 @@
  */
 
 using UdonSharp;
-
 using UnityEngine;
-
 using VRC.SDKBase;
 using VRC.Udon;
 using VRC.SDK3.Data;
@@ -16,59 +14,59 @@ namespace OpenFlightVRC
     /// <summary>
     /// A Base class that allows for callbacks to be added and run
     /// </summary>
-    /// <typeparam name="T">The type of the callback ID. This should be an enum</typeparam>
-    public class CallbackUdonSharpBehaviour<T> : LoggableUdonSharpBehaviour where T : Enum
+    /// <typeparam name="EnumType">The type of the callback ID. This should be an enum</typeparam>
+    public class CallbackUdonSharpBehaviour<EnumType> : CallbackUdonSharpBehaviourDataStorage where EnumType : Enum
     {
-        /// <summary>
-        /// Stores all the data on callbacks
-        /// </summary>
-        /// <remarks> 
-        /// This is a dictionary of all the callbacks that are currently being used. The key is the name of the callback, and the value is a key-value pairs, with the key being the object and the value being the method name
-        /// </remarks>
-        private DataDictionary _callbackData = new DataDictionary();
+        //FOREWARNING: There is some stupid as fuck behaviour going on in here.
+        //Normally, you would want to put any variables you are using in the same class file you are using them in
+        //BUT, due to a U# bug related to generic classes, I need to put them in a inherited class that doesnt spawn multiple versions
+        //of itself. If they were to be put in this level, U# would complain about symbols being duplicated due to the generic class being expanded
 
-        /// <inheritdoc cref="AddCallback(Enum, UdonSharpBehaviour, string)"/>
-        public bool AddCallback(T callbackID, UdonSharpBehaviour behaviour, params string[] methodName)
-        {
-            bool success = true;
-            for (int i = 0; i < methodName.Length; i++)
-            {
-                success &= AddCallback(callbackID, behaviour, methodName[i]);
-            }
-            return success;
-        }
+        //TLDR, any variables to inject ***CANNOT*** be defined in here, and instead need to be defined in the parent class
 
         /// <summary>
         /// Adds a callback based on the callback ID
         /// </summary>
         /// <param name="callbackID">The ID of the callback to add. This should be an enum</param>
         /// <param name="behaviour">The behaviour to run the callback on. Use <see cref="this"/> to run the callback on the behaviour this script is attached to</param>
-        /// <param name="methodName">The name of the method to run when this callback is triggered. Recommended to use <see cref="nameof"/> to get the method name</param>
+        /// <param name="methodNames">The name of the method to run when this callback is triggered. Recommended to use <see cref="nameof"/> to get the method name</param>
         /// <returns>Returns a true if the callback was added successfully, and false if the callback already exists</returns>
-        public bool AddCallback(T callbackID, UdonSharpBehaviour behaviour, string methodName)
+        public bool AddCallback(EnumType callbackID, UdonSharpBehaviour behaviour, params string[] methodNames)
         {
-            int id = Convert.ToInt32(callbackID);
+            bool success = true;
+            int id;
 
-            //add the id dictionary, only making it if it doesn't exist
-            if (!_callbackData.ContainsKey(id))
-                _callbackData.Add(id, new DataDictionary());
-
-            //add the behaviour key, only making it if it doesn't exist
-            if (!_callbackData[id].DataDictionary.ContainsKey(behaviour))
-                _callbackData[id].DataDictionary.Add(behaviour, new DataList());
-
-            //add the method name, only if its not already there
-            if (!_callbackData[id].DataDictionary[behaviour].DataList.Contains(methodName))
+            for (int i = 0; i < methodNames.Length; i++)
             {
-                //Logger.Log(String.Format("Added callback [{0}] for [{1}]", Logger.ColorizeFunction(behaviour, methodName), Logger.ColorizeFunction(behaviour, methodName)), this);
-                _callbackData[id].DataDictionary[behaviour].DataList.Add(methodName);
+                id = Convert.ToInt32(callbackID);
+
+                //add the id dictionary, only making it if it doesn't exist
+                if (!_callbackData.ContainsKey(id))
+                {
+                    _callbackData.Add(id, new DataDictionary());
+                }
+
+                //add the behaviour key, only making it if it doesn't exist
+                if (!_callbackData[id].DataDictionary.ContainsKey(behaviour))
+                {
+                    _callbackData[id].DataDictionary.Add(behaviour, new DataList());
+                }
+
+                //add the method name, only if its not already there
+                if (!_callbackData[id].DataDictionary[behaviour].DataList.Contains(methodNames[i]))
+                {
+                    //Logger.Log(String.Format("Added callback [{0}] for [{1}]", Logger.ColorizeFunction(behaviour, methodName), Logger.ColorizeFunction(behaviour, methodName)), this);
+                    _callbackData[id].DataDictionary[behaviour].DataList.Add(methodNames[i]);
+                }
+                else
+                {
+                    Logger.LogWarning(String.Format("[{0}] Tried to register a callback for [{1}] that already exists!", behaviour.name, methodNames[i]), this);
+                    return false;
+                }
+                success &= true;
             }
-            else
-            {
-                Debug.LogWarning(String.Format("[{0}] Tried to register a callback for [{1}] that already exists!", behaviour.name, methodName));
-                return false;
-            }
-            return true;
+
+            return success;
         }
 
         /// <summary>
@@ -76,7 +74,7 @@ namespace OpenFlightVRC
         /// </summary>
         /// <inheritdoc cref="AddCallback(Enum, UdonSharpBehaviour, string)"/>
         /// <returns>Returns a true if the callback was removed successfully, and false if the callback does not exist</returns>
-        public bool RemoveCallback(T callbackID, UdonSharpBehaviour behaviour, string methodName)
+        public bool RemoveCallback(EnumType callbackID, UdonSharpBehaviour behaviour, string methodName)
         {
             int id = Convert.ToInt32(callbackID);
             //check if the callback exists
@@ -97,7 +95,7 @@ namespace OpenFlightVRC
                     }
                 }
             }
-            Debug.LogWarning(String.Format("[{0}] Tried to remove a callback for [{1}] that does not exist!", behaviour.name, methodName));
+            Logger.LogWarning(String.Format("[{0}] Tried to remove a callback for [{1}] that does not exist!", behaviour.name, methodName), this);
             return false;
         }
 
@@ -105,7 +103,7 @@ namespace OpenFlightVRC
         /// Runs all the callbacks for the given callback ID
         /// </summary>
         /// <param name="callbackID"><inheritdoc cref="AddCallback(Enum, UdonSharpBehaviour, string)" path="/param[@name='callbackID']"/></param>
-        internal void RunCallback(T callbackID)
+        internal void RunCallback(EnumType callbackID)
         {
             int id = Convert.ToInt32(callbackID);
             //check if the callback exists
@@ -134,14 +132,14 @@ namespace OpenFlightVRC
                         //check if the behaviour is null, and if it is, remove it
                         if (behaviour == null)
                         {
-                            Debug.LogWarning(String.Format("Behaviour for callback [{0}] is null, removing callback", methodName));
+                            Logger.LogWarning(String.Format("Behaviour for callback [{0}] is null, removing callback", methodName), this);
                             methods.Remove(methodName);
                             continue;
                         }
 
                         //run the method
                         behaviour.SendCustomEvent(methodName);
-                        Debug.Log(String.Format("Running callback [{0}] for [{1}]", methodName, behaviour));
+                        Logger.Log(String.Format("Running callback [{0}] for [{1}]", methodName, behaviour), this);
                     }
                 }
             }
---
+++ Packages/com.mattshark.openflight/Runtime/Scripts/Utils/CallbackUdonSharpBehaviourDataStorage.cs
@@ -0,0 +1,27 @@
+/**
+ * @ Maintainer: Happyrobot33
+ */
+
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.SDK3.Data;
+using System;
+
+namespace OpenFlightVRC
+{
+    /// <summary>
+    /// This base class requirement is really stupid, but it is required for the CallbackUdonSharpBehaviour to work without U# throwing a bug that will likely never be fixed
+    /// </summary>
+    public class CallbackUdonSharpBehaviourDataStorage : LoggableUdonSharpBehaviour
+    {
+        /// <summary>
+        /// Stores all the data on callbacks
+        /// </summary>
+        /// <remarks> 
+        /// This is a dictionary of all the callbacks that are currently being used. The key is the name of the callback, and the value is a key-value pairs, with the key being the object and the value being the method name
+        /// </remarks>
+        private protected DataDictionary _callbackData = new DataDictionary();
+    }
+}
