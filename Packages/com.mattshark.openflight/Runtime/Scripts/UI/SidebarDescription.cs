da48fa0f8edf453d72d730157167e428aa40a77d
Happyrobot33 <matthewherber@sbcglobal.net>
2024-12-10 10:03:36 -0500

    Button Varieties, automate event setup

--- Packages/com.mattshark.openflight/Runtime/Scripts/UI/SidebarDescription.cs
+++ Packages/com.mattshark.openflight/Runtime/Scripts/UI/SidebarDescription.cs
@@ -1,7 +1,6 @@
 ﻿
 using UdonSharp;
 
-
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -10,43 +9,45 @@ using VRC.Udon;
 
 namespace OpenFlightVRC.UI
 {
-/*     #if UNITY_EDITOR
-using UnityEditor;
+    using UdonSharpEditor;
+
+#if UNITY_EDITOR
+    using UnityEditor;
+    using UnityEditor.Events;
 
     using UnityEngine.Events;
 
+    using VRC.Udon.Common.Interfaces;
+
     [CustomEditor(typeof(SidebarDescription))]
-public class SidebarDescriptionEditor : Editor
-{
-    //we want to automatically setup the EventTrigger component
-    public override void OnInspectorGUI()
+    public class SidebarDescriptionEditor : Editor
     {
-        base.OnInspectorGUI();
-        
-        SidebarDescription target = (SidebarDescription)this.target;
-
-        //check if the EventTrigger component exists
-        if (target.GetComponent<EventTrigger>() == null)
+        //we want to automatically setup the EventTrigger component
+        public override void OnInspectorGUI()
         {
-            //add the EventTrigger component
-            target.gameObject.AddComponent<EventTrigger>();
+            base.OnInspectorGUI();
 
-        }
-        //setup the EventTrigger component
-        var trigger = target.GetComponent<EventTrigger>();
+            SidebarDescription target = (SidebarDescription)this.target;
 
-        //list all delegates
-        var delegates = trigger.triggers;
-        foreach (var delegateEntry in delegates)
-        {
-            Debug.Log(delegateEntry.eventID);
-            Debug.Log(delegateEntry.callback);
+            //check if the EventTrigger component exists
+            if (target.GetComponent<EventTrigger>() == null)
+            {
+                //add the EventTrigger component
+                target.gameObject.AddComponent<EventTrigger>();
 
-            //
+            }
+            //setup the EventTrigger component
+            var trigger = target.GetComponent<EventTrigger>();
+            //clear the triggers
+            trigger.triggers.Clear();
+            EventTrigger.Entry entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.PointerEnter;
+            UnityAction<string> baseEvent = UdonSharpEditorUtility.GetBackingUdonBehaviour(target).SendCustomEvent;
+            UnityEventTools.AddStringPersistentListener(entry.callback, baseEvent, nameof(SidebarDescription.HoverEnter));
+            trigger.triggers.Add(entry);
         }
     }
-}
-#endif */
+#endif
 
     [RequireComponent(typeof(EventTrigger))]
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
