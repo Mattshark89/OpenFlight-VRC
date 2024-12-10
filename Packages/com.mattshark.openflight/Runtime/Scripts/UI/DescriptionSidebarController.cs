3bfc85f6626bd5468d5dd8b1eac606b830b18a46
Happyrobot33 <matthewherber@sbcglobal.net>
2024-12-10 08:57:58 -0500

    Initial hover system

---
+++ Packages/com.mattshark.openflight/Runtime/Scripts/UI/DescriptionSidebarController.cs
@@ -0,0 +1,49 @@
+﻿
+using TMPro;
+
+using UdonSharp;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace OpenFlightVRC.UI
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    [DefaultExecutionOrder(-1000)]
+    public class DescriptionSidebarController : UdonSharpBehaviour
+    {
+        internal const string EXPECTEDNAME = "DescriptionSidebarController";
+        public TextMeshProUGUI titleText;
+        public TextMeshProUGUI descriptionText;
+        void Start()
+        {
+            name = EXPECTEDNAME;
+        }
+
+        void OnEnable()
+        {
+            SetTitle("Info");
+            SetDescription("Hover over something to get a description of what it does!");
+        }
+
+        internal void SetDescription(string description)
+        {
+            descriptionText.text = description;
+            UpdateLayout();
+        }
+
+        internal void SetTitle(string title)
+        {
+            titleText.text = title;
+            UpdateLayout();
+        }
+
+        internal void UpdateLayout()
+        {
+            LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)transform);
+        }
+    }
+}
---
+++ Packages/com.mattshark.openflight/Runtime/Scripts/UI/SidebarDescription.cs
@@ -0,0 +1,69 @@
+﻿
+using UdonSharp;
+
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace OpenFlightVRC.UI
+{
+/*     #if UNITY_EDITOR
+using UnityEditor;
+
+    using UnityEngine.Events;
+
+    [CustomEditor(typeof(SidebarDescription))]
+public class SidebarDescriptionEditor : Editor
+{
+    //we want to automatically setup the EventTrigger component
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        
+        SidebarDescription target = (SidebarDescription)this.target;
+
+        //check if the EventTrigger component exists
+        if (target.GetComponent<EventTrigger>() == null)
+        {
+            //add the EventTrigger component
+            target.gameObject.AddComponent<EventTrigger>();
+
+        }
+        //setup the EventTrigger component
+        var trigger = target.GetComponent<EventTrigger>();
+
+        //list all delegates
+        var delegates = trigger.triggers;
+        foreach (var delegateEntry in delegates)
+        {
+            Debug.Log(delegateEntry.eventID);
+            Debug.Log(delegateEntry.callback);
+
+            //
+        }
+    }
+}
+#endif */
+
+    [RequireComponent(typeof(EventTrigger))]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class SidebarDescription : UdonSharpBehaviour
+    {
+        public string title;
+        public string description;
+        private DescriptionSidebarController sidebar;
+        void Start()
+        {
+            sidebar = GameObject.Find(DescriptionSidebarController.EXPECTEDNAME).GetComponent<DescriptionSidebarController>();
+        }
+
+        public void HoverEnter()
+        {
+            sidebar.SetTitle(title);
+            sidebar.SetDescription(description);
+        }
+    }
+}
